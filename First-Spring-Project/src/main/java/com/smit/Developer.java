05bd38df3c1d2323a63763026e2b70558847da23
Smit Roy <ttssmitherio@gmail.com>
2025-09-09 09:39:36 +0530

    Add files via upload

---
+++ First-Spring-Project/src/main/java/com/smit/Developer.java
@@ -0,0 +1,10 @@
+package com.smit;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Developer {
+    public void Method(){
+        System.out.println("Developing....");
+    }
+}
